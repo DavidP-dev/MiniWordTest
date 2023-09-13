6587e1587bd0e9c178fdc5b564587bd9c74c0309
DavidP-dev <picha.mda@seznam.cz>
2023-09-13 13:01:14 +0200

    Initial commit

---
+++ MiniWordTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using MiniSoftware;
+
+var destinationPath = @"Fotodokumentace.docx";
+var templatePath = @"Test.docx";
+var photoPath = @"Foto č. 2.jpg";
+
+var value = new Dictionary<string, object>()
+{
+    ["image"] = new MiniWordPicture() { Path = photoPath, Height = 360 }
+};
+        
+MiniWord.SaveAsByTemplate(destinationPath, templatePath, value);
